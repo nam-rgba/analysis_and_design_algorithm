bc029d0af3e62cafc80c4c463b00ac5d5833553f
Nam <doanngocnam3002@gmail.com>
2023-11-29 16:25:27 +0700

    done

--- Final/apriori.py
+++ Final/apriori.py
@@ -1,31 +1,50 @@
-from scipy.stats import norm, t
-from itertools import product, combinations;
-import math
+from scipy.stats import t
+from itertools import product
+import csv
+import random
+import ast
+from collections import Counter
+
 
 # Main algorithms
-def wPFI_Apriori(DB, w, msup, t , alpha):
+def wPFI_Apriori(DB: list, w: dict, msup:int, t:float , alpha:float)->list:
+    # Function to find w-PFI
+    # Input: 
+        # DB: list of transactions
+        # w: dictionary of weights
+        # msup: minimum support
+        # t: threshold
+        # alpha: confidence
+    # Output:
+        # WPFI: list of w-PFI itemsets
+
+
     # create set of all item
-    I=set(item for transaction in DB for item in transaction)
-    tempCk=[I]
-    print('I:',I)
-    WPFI=[[]]
+    I:set=set(item for transaction in DB for item in transaction)
+    tempCk: list=[I]
+    WPFI: list=[[]]
+    mu:list=[[]]
 
     # Check if I is w-PFI or not
-    WPFI1, mu1=Scan_Find_Size_k_wFPI(tempCk, DB, w, msup, t)
+
+    WPFI1, mu1=Scan_Find_Size_k_wFPI(DB, tempCk, w, msup, t,2)
     print('wpi1',WPFI1)
     WPFI.append(WPFI1)
-    k=2
+    mu.append(mu1)
+    k: int=2
 
-    # # Cause anti-monotonicity of w-PFI, at least one itemset size k-1 must be w-PFI
-    # while WPFI[k-1]!=[]:
-    #     # Step 6: Generate size-k wPFI
-
-    #     # Step 7: Scan and find size-k wPFI
-    #     WPFIk, mu1 = Scan_Find_Size_k_wFPI(WPFI[k-1], DB, w, msup, t,k-1)
+    # Cause anti-monotonicity of w-PFI, at least one itemset size k-1 must be w-PFI
+    while WPFI[k-1]!=[]:
+        # Step 6: Generate size-k wPFI
+        # Ck=wPFI_Apriori_Gen(WPFI[k-1], I, mu, w, alpha, len(DB), msup, t)
+        Ck=exact_apriori(WPFI[k-1], I, w, t)
+        # Step 7: Scan and find size-k wPFI
+        WPFIk, muk = Scan_Find_Size_k_wFPI(DB,Ck, w, msup, t,2)
         
-    #     # Step 8: Add size-k wPFI to WPFI
-    #     WPFI.append(WPFIk)
-    #     k += 1
+        # Step 8: Add size-k wPFI to WPFI
+        WPFI.append(WPFIk)
+        mu=muk
+        k += 1
 
     return WPFI
 
@@ -33,122 +52,300 @@ def wPFI_Apriori(DB, w, msup, t , alpha):
 
 
 
-# Function to generate all subsets of a set with size k, algorithm 3
-def wPFI_Apriori_Gen(WPFI_k_minus_1, I, muk_minus_1, w, alpha, n, msup, t):
-    # Initialize
-    Ck = []
-    m = max(w.values())
+def wPFI_Apriori_Gen(WPFI_k_minus_1: list, I:set, mu:list, w:dict, alpha:float, n:int, msup:int, t:float)->list:
+    # Function to generate candidates for wPFI using Apriori framework, algorithm 3
+    # Input: 
+        # WPFI_k_minus_1: list of w-PFI itemsets of size k-1
+        # I: set of all items
+        # mu: list of mean U
+        # w: dictionary of weights
+        # alpha: confidence
+        # n: number of transactions
+        # msup: minimum support
+        # t: threshold
+    # Output: List of candidates
 
-    mu_hat = msup-1
+
+    # Initialize
+    Ck:list = [] # List of (set) candidate itemsets
+    m:float = max(w.values())
+    mu_hat:float = msup-1
 
     
     # init I'
-    I_prime={i for i in I if any(i.issubset(Y) for Y in WPFI_k_minus_1)}
-    print('I_prime: ',WPFI_k_minus_1)	
+    I_prime={i for i in I if i  in WPFI_k_minus_1} # I', set of itemsets with at least one item in WPFI_k_minus_1
 
     # Loop
     for X in WPFI_k_minus_1:
-        for i in (I_prime-{X}):
+        print('X line 52:' ,X)
+        for i in (I_prime-X):
             # Check if w(X ∪ Ii) ≥ t
             if calculate_weighted_support(X.union(i),w) >= t:
-                if min()>=mu_hat  :
+                if min(mu[WPFI_k_minus_1.index(X)], mu[I_prime.index(i)])>=mu_hat and  mu[WPFI_k_minus_1.index(X)]* mu[I_prime.index(i)]>=alpha*n*mu_hat :
+                    # Check the conditions for adding X ∪ Ii to Ck
                     Ck.add(X.union(i))
         #  Find the item I_m with the minimum weight in X
-        I_m=min(X, key=lambda x:w[x])
-        for i in (I-I_prime-{X}):
+        I_m=min(X, key=lambda x:calculate_weighted_support({x}, w))
+        for i in (I-I_prime-X):
             # Check if w(X ∪ Ii) ≥ t and w(Ii) < w(I_m)
-            if calculate_weighted_support(X.union(i), w)>=t and w[i]<w[I_m]:
-                Ck.add(X.union(i))
-
+            if calculate_weighted_support(X.union(i), w)>=t and calculate_weighted_support(i, w)<w[I_m]:
+                if min(mu[WPFI_k_minus_1.index(X)], mu[I_prime.index(i)])>=mu_hat and  mu[WPFI_k_minus_1.index(X)]* mu[I_prime.index(i)]>=alpha*n*mu_hat:
+                    # Check the conditions for adding X ∪ Ii to Ck
+                    Ck.add(X.union(i))
+    print('Ck: ',Ck)
     return Ck
 
 
+def exact_apriori(WPFI_k_minus_1: list, I: set, w: dict, t: float):
+    # Function to generate candidates for wPFI using Apriori framework, algorithm 2
+    # Input:
+        # WPFI_k_minus_1: list of w-PFI itemsets of size k-1
+        # I: set of all items
+        # w: dictionary of weights
+        # t: threshold
+    # Output: List of candidates
 
-# Helper function to generate all subsets of a set with size k
+    ck:list=[]
+    I_prime:set = {i for Y in WPFI_k_minus_1 for i in I if i in Y}
+    # I', set of itemsets with at least one item in WPFI_k_minus_1
+    for X in WPFI_k_minus_1:
+        for i in I_prime-X:
+            if calculate_weighted_support(X.Union(i),w)>=t:
+                ck.add(X.union(i))
+        #  Find the item I_m with the minimum weight in X
+        I_m=min(X, key=lambda x:calculate_weighted_support({x}, w))
+        for i in I-I_prime-X:
+            if calculate_weighted_support(X.Union(i))>=t and calculate_weighted_support({i},w)<=w[I_m]:
+                ck.add(X.union(i))
 
-def powerset(s, k):
-    result = [set(combination) for combination in combinations(s, k)]
-    print(result)
-    return result
+    return ck
 
-def calculate_weighted_support(candidate, w):
-    print("cate",candidate)
+def calculate_weighted_support(candidate: list, w: dict)->float:
     # Helper function to calculate the weighted support of a candidate
+    # Input: 
+        # candidate: list of items
+        # w: dictionary of weights
+    # Output: avaerage weight of the candidate
+
     return sum(w[item] for item in candidate)/len(candidate)
 
 
 
-# Helper function to calculate the support of an itemset in a transaction
-def calculate_support(itemset, transactions):
+def calculate_support(itemset:set, transactions:list)->float:
+    # Helper function to calculate the support of an itemset in a transaction
+    # Input:
+        # itemset: list of items
+        # transactions: list of transactions
+    # Output: support of the itemset
     # Loop and count the number of times the itemset appears in the transaction
     for transaction in transactions:
-        count=sum(1 for item in itemset if item in transaction)/len(transactions)
+        count: float=sum(1 for item in itemset if item in transaction)/len(transactions)
     return count
 
 
 
-#Function to calculate the probability Pr(sup(X)>=msup)
-def calculate_probability(X, msup, database):
-    n = len(database)
-    possible_worlds = []
+def calculate_probability(X: set, msup: int, database:list)->float:
+    # Function to calculate the probability Pr(sup(X)>=msup)
+    # Input:
+        # X: set of items
+        # msup: minimum support
+        # database: list of transactions
+    # Output: probability Pr(sup(X)>=msup)
+
+
+    n:int = len(database)
+    possible_worlds:list = []
     
     # Generate all possible worlds
     for i in product(*[[(item, 1 - database[j][item]) for item in database[j]] for j in range(n)]):
         possible_worlds.append(list(i))
 
     # Find worlds where X is a subset and meets the support condition
-    satisfying_worlds = [world for world in possible_worlds if all(item in set(X) for item, _ in world) and sum(1 for _, prob in world if prob > 0) >= msup]
+    satisfying_worlds:list = [world for world in possible_worlds if all(item in set(X) for item, _ in world) and sum(1 for _, prob in world if prob > 0) >= msup]
 
     # Calculate the probability for each satisfying world
-    probabilities = [eval('*'.join(map(str, [1 - prob for _, prob in world]))) for world in satisfying_worlds]
+    probabilities:list = [eval('*'.join(map(str, [1 - prob for _, prob in world]))) for world in satisfying_worlds]
 
     # Calculate the total probability
-    total_probability = sum(probabilities)
+    total_probability:float = sum(probabilities)
 
     return total_probability
 
 
 def is_weighted_probabilistic_frequent_itemset(itemset, weights, transactions, msup, t):
-    print('itemset: ',itemset)
     # Helper function to check if a candidate is a weighted probabilistic frequent itemset
+    # Input:
+        # itemset: candidate
+        # weights: dictionary of weights
+        # transactions: list of transactions
+        # msup: minimum support
+        # t: threshold
+    # Output: boolean to check if Ck is a weighted probabilistic frequent itemset
+
     weight=calculate_weighted_support(itemset, weights)
     support=calculate_probability(itemset, msup, transactions)
-    print('support: ',support)
     # Pr(sup(X)>=msup)>=t/w(X)
     return weight*support>=t , support
 
 
-# Find size-k wPFI based on definition 5
-def Scan_Find_Size_k_wFPI(Ck, DB, w, msup, t):
+def Scan_Find_Size_k_wFPI( DB: list,Ck: list, w: dict, msup:int, t: float,prop: int)->list:
+    # Function to find size-k wPFI based on definition 5
+    # Input:
+        # DB: list of transactions
+        # Ck: candidates
+        # w: dictionary of weights
+        # msup: minimum support
+        # t: threshold
+        # prop: 1 or 2, flag to choose which algorithm to use
+    # Output: List of frequent itemsets that are wPFI
     # Initialize
-    frequent_itemsets=[]
-    muk=[]
+    frequent_itemsets: list=[]
+    muk:list=[]
 
     for itemset in Ck:
-        isW, mu = is_weighted_probabilistic_frequent_itemset(itemset, w, DB, msup, t)
-        if isW:
-            frequent_itemsets.append(itemset)
-            muk.append(mu)
+        # If flag 1, use is_weighted_probabilistic_frequent_itemset
+        if prop==1:
+            isW, mu = is_weighted_probabilistic_frequent_itemset(itemset,w, DB, msup,t)
+            if isW:
+                frequent_itemsets.append(itemset)
+                muk.append(mu)
+        # If flag 2, use isWPFI
+        if prop==2:
+            isW, mu = isWPFI(itemset, DB, msup)
+            if isW:
+                frequent_itemsets.append(itemset)
+                muk.append(mu)
 
     return frequent_itemsets, muk
 
+
+
+def meanU(X: set, DB:list)->float:
+    # Function to calculate the mean support of an itemset
+    # Input:
+        # X: set of items
+        # DB: list of transactions
+    # Output: mean support
+    result:float=0
+    for transcation in DB:
+        if X.issubset(transcation.keys()):
+            pr=1
+            for x in X:
+                pr*=transcation[x]
+            result+=pr
+    return result
+
+def isWPFI(Ck:set, DB:list, msup:int)->bool:
+    # Helper function to check if a candidate is a weighted probabilistic frequent itemset
+    # Input:
+        # Ck: candidate
+        # DB: transaction database
+        # msup: minimum support
+    # Output: boolean to check if Ck is a weighted probabilistic frequent itemset and mu
+    mu:float = meanU(Ck, DB)
+    mu_hat:int =msup-1
+    return mu>=mu_hat, mu
+
+
+
+    try:
+        with open(input_file_path, 'r') as infile, open(output_file_path, 'w', newline='') as outfile:
+            reader = csv.reader(infile, delimiter=' ')
+            writer = csv.writer(outfile, delimiter=',')
+
+            for row in reader:
+                # Convert each item in the row to a dictionary with a random float value
+                processed_row = {item: round(random.uniform(0.1, 1.0), 2) for item in row}
+                
+                # Write the processed row to the output file
+                writer.writerow(processed_row.values())
+
+    except FileNotFoundError:
+        print(f"File not found: {input_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 # Transaction Database
 DB=[
     {'milk':0.4,'fruit':1.0,'video':0.3},
     {'milk':1.0,'fruit':0.8}
 ]
+X={'milk', 'fruit'}
+print(meanU(X, DB))
 # Weights table
 w={'milk':0.4, 'fruit':0.9, 'video':0.6}
 # Minimum support
-msup=2
+msup=1
 # Threshold
 t=0.2
 # Scale factor
 alpha=0.2
 
 
-result = wPFI_Apriori(DB, w, msup, t, alpha)
+def read_first_100_rows(input_file_path):
+    
+    try:
+        data_list = []
+
+        with open(input_file_path, 'r') as infile:
+            reader = csv.reader(infile, delimiter=' ')
+
+            # Read the first 10 rows
+            for _ in range(10):
+                row = next(reader, None)
+                if row is not None:
+                    data_list.append(ast.literal_eval(row[0]))
+
+        return data_list
+
+    except FileNotFoundError:
+        print(f"File not found: {input_file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def read_weight_file(file_path):
+    try:
+        weight_dict = {}
+
+        # Read the weights from the file
+        with open(file_path, 'r') as file:
+            for line in file:
+                # Split the line into item and weight
+                
+                item_str, weight_str = line.strip().split(':')
+                item = int(item_str.strip())
+                weight = float(weight_str)
+                # item=int(item)
+                
+                # Add the item and weight to the dictionary
+                weight_dict[item] = round(weight*10,2)
+
+        return weight_dict
+
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+
+
+weight_file_path = 'weight.csv' 
+weight_dictionary = read_weight_file(weight_file_path) 
+input_file_path = 'sample.csv'  
+first_100_rows = read_first_100_rows(input_file_path)
+
+
+
+
+
+
+result = wPFI_Apriori(first_100_rows, weight_dictionary, msup, t, alpha)
+print(result)
 
 
 
---
+++ Final/data.py
@@ -0,0 +1,73 @@
+import csv
+import random
+
+def process_transaction_data(input_file_path, output_file_path):
+    try:
+        with open(input_file_path, 'r') as infile, open(output_file_path, 'w', newline='') as outfile:
+            reader = csv.reader(infile, delimiter=' ')
+            writer = csv.writer(outfile, delimiter=',')
+
+            for row in reader:
+                # Filter out empty strings and convert each item to a dictionary with a random float value
+                processed_row = {int(item): round(random.uniform(0.1, 1.0), 2) for item in row if item}
+                
+                # Write the processed row to the output file
+                writer.writerow([processed_row])
+
+    except FileNotFoundError:
+        print(f"File not found: {input_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Example usage
+input_file_path = 'connect.txt'  # Replace with your input file path
+output_file_path = 'sample.csv'  # Replace with your desired output file path
+
+# process_transaction_data(input_file_path, output_file_path)
+
+from collections import Counter
+
+def generate_weight_table(input_file_path, output_file_path, delimiter=' ', lines_to_read=100):
+    try:
+        item_counts = Counter()
+
+        # Read the first 100 lines from the file
+        with open(input_file_path, 'r') as file:
+            for _ in range(lines_to_read):
+                line = file.readline()
+                if not line:
+                    break  # Break if end of file is reached
+                items = line.strip().split(delimiter)
+                item_counts.update(items)
+
+        # Calculate the total number of transactions
+        total_transactions = sum(item_counts.values())
+
+        # Generate the weight table (dictionary)
+        weight_table = {item: count / total_transactions for item, count in item_counts.items()}
+
+        # Write the result to the output file
+        with open(output_file_path, 'w') as output_file:
+            for item, weight in weight_table.items():
+                output_file.write(f"{item}: {weight}\n")
+
+        return weight_table
+
+    except FileNotFoundError:
+        print(f"File not found: {input_file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+# Example usage
+input_file_path = 'connect.txt'  # Replace with your actual file path
+output_file_path = 'weight.csv'  # Replace with your desired output file path
+lines_to_read = 10
+
+weight_table = generate_weight_table(input_file_path, output_file_path, lines_to_read=lines_to_read)
+
+# Print the generated weight table
+print("Weight Table:")
+for item, weight in weight_table.items():
+    print(f"{item}: {weight}")
